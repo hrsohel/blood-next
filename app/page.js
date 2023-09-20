8fcb968cf0c1a37a092d2694dd324b98f53dc28d
Sohel <hrsohel679@gmail.com>
2023-09-20 12:49:56 +0600

    home image changed

--- app/page.js
+++ app/page.js
@@ -6,29 +6,38 @@ import React from "react";
 const page = () => {
   return (
     <main className="flex h-screen w-full items-start justify-center">
+      {/* <div className="md:w-[50rem] ">
+        <Image
+          src="/images\blood-image1.png"
+          className="w-full h-full object-cover"
+          width="1000"
+          height="0"
+          alt="Home image"
+        />
+      </div>
+      <div className="px-4 md:w-[50rem]">
+        <h1 className="md:text-xl text-sm">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi
+          distinctio, esse maxime ea totam ducimus veniam unde commodi facere
+          architecto, vero perspiciatis ratione molestias aliquid sint ex,
+          expedita aperiam laboriosam!
+        </h1>
+      </div> */}
       <Navbar />
       <section className="overflow-y-scroll h-full w-[95%] relative ">
-        <div className="h-full">
+        <div className="f-full">
           <Image
-            src="/images\blood-bank.png"
+            src="/images/blood-image1.png"
             className="w-full h-full object-cover"
             width="1000"
-            height="0"
+            height="1000"
             alt="Home image"
           />
         </div>
-        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-          <h1
-            style={{ textShadow: "2px 2px 1rem #fff" }}
-            className="text-4xl text-center text-red-500 font-bold"
-          >
-            Spare a Moment, Save a Life: Donate Blood Today!
-          </h1>
-        </div>
         <div className="flex my-2 px-4 items-center justify-center md:flex-nowrap flex-wrap">
-          <div>
+          <div className="md:w-[50%]">
             <h1 className="text-2xl">BLOOD GROUPS</h1>
-            <p className="text-lg">
+            <p className="md:text-lg text-sm ">
               blood group of any human being will mainly fall in any one of the
               following groups.
             </p>
@@ -38,7 +47,7 @@ const page = () => {
               <li>O positive or O negative</li>
               <li>AB positive or AB negative</li>
             </ul>
-            <p className="text-lg">
+            <p className="md:text-lg text-sm">
               A healthy diet helps ensure a successful blood donation, and also
               makes you feel better! Check out the following recommended foods
               to eat prior to your donation.
@@ -46,7 +55,7 @@ const page = () => {
           </div>
           <div className="w-full md:w-1/2">
             <Image
-              src="/images\bloodDonor.jpg"
+              src="/images\blood-image2.png"
               className="w-full object-cover h-full"
               width="1000"
               height="1000"
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,31 +1,58 @@
+"use client";
 import Link from "next/link";
 import React from "react";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const path = usePathname();
   return (
     <>
       <header className="flex relative min-h-screen items-center flex-col w-[5%] p-4 bg-[#333]">
-        <Link title="Home" href="/">
-          <i className="fa fa-home text-2xl lg:text-4xl my-2 text-white font-bold"></i>
+        <Link
+          title="Home"
+          className={`${path === "/" ? "text-red-500" : "text-white"}`}
+          href="/"
+        >
+          <i className="fa fa-home text-2xl lg:text-4xl my-2  font-bold"></i>
         </Link>
-        <Link title="About us" href="/about">
-          <i className="fa fa-info-circle text-2xl lg:text-4xl my-2 text-white font-bold"></i>
+        <Link
+          className={`${path === "/about" ? "text-red-500" : "text-white"}`}
+          title="About us"
+          href="/about"
+        >
+          <i className="fa fa-info-circle text-2xl lg:text-4xl my-2  font-bold"></i>
         </Link>
-        <Link title="Become a donor" href="register">
-          <i className="fa fa-user-plus text-2xl lg:text-4xl my-2 text-white font-bold"></i>
+        <Link
+          className={`${path === "/register" ? "text-red-500" : "text-white"}`}
+          title="Become a donor"
+          href="/register"
+        >
+          <i className="fa fa-user-plus text-2xl lg:text-4xl my-2 font-bold"></i>
         </Link>
-        <Link title="Search donor" href="/search-donor">
-          <i className="fa fa-search text-2xl lg:text-4xl my-2 text-white font-bold"></i>
+        <Link
+          className={`${
+            path === "/search-donor" ? "text-red-500" : "text-white"
+          }`}
+          title="Search donor"
+          href="/search-donor"
+        >
+          <i className="fa fa-search text-2xl lg:text-4xl my-2 font-bold"></i>
         </Link>
-        <Link title="Login" href="/login">
-          <i className="fa fa-sign-in text-2xl lg:text-4xl my-2 text-white font-bold"></i>
+        <Link
+          className={`${path === "/login" ? "text-red-500" : "text-white"}`}
+          title="Login"
+          href="/login"
+        >
+          <i className="fa fa-sign-in text-2xl lg:text-4xl my-2 font-bold"></i>
         </Link>
         <Link
           title="Profile"
           href="/user"
-          className="absolute bottom-4 left-2 md:left-4"
+          className={`${
+            path === "/user" ? "text-red-500" : "text-white"
+          } absolute bottom-4 left-2 md:left-4`}
         >
-          <i className="fa fa-user text-2xl lg:text-4xl my-2 text-white font-bold"></i>
+          <i className="fa fa-user text-2xl lg:text-4xl my-2 font-bold"></i>
         </Link>
       </header>
     </>
